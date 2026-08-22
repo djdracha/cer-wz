66646f8920f65d75000704271ba97e59c610a112
djdracha <djdracha@gmail.com>
2026-08-22 11:42:06 +0200

    Change default points from 100 to 1 for players

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def get_state():
     player_data = {
         'username': username,
         'hand': game_state['players'].get(username, {}).get('hand', []),
-        'points': game_state['players'].get(username, {}).get('points', 100),
+        'points': game_state['players'].get(username, {}).get('points', 1),
         'phase': game_state['phase'],
         'current_player': game_state['current_player'],
         'players': players_data,
